d0a91ada02e9dce5cb64dd277cb8cd77feca570b
devleonny <166626175+devleonny@users.noreply.github.com>
2025-09-16 09:55:32 -0300

    feat: orçamentos

--- js/orcamentos.js
+++ js/orcamentos.js
@@ -1,32 +1,55 @@
 const voltarOrcamentos = `<button style="background-color: #3131ab;" onclick="telaOrcamentos()">Voltar</button>`
 let ambientes = {
-    'Quarto': [...povoarLista(0, 5, 'Quarto Piso'),
-        ...povoarLista(1, 5, 'Quarto Piso')],
-    'Wc': povoarLista(0, 5, 'Wc Social'),
-    'Wc Suite': [...povoarLista(0, 5, 'Wc Suite - Quarto Piso'),
-        ...povoarLista(1, 5, 'Wc Suite - Quarto Piso')],
-    'Varanda': [],
-    'Cozinha': [],
-    'Dispensa': [],
-    'Corredor': [],
-    'Hall de Entrada': [],
-    'Lavandaria': [],
-    'Terraço': [],
-    'Arrecadação': [],
-    'Anexo': [],
-    'Casa das Máquinas': [],
-    'Escritório': [],
-    'Sótão': [],
-    'Escada': [],
-    'Garagem': [],
-    'Jardim': [],
-    'Telhado': [],
-    'Telheiro': [],
-    'Piscina Interior': [],
-    'Piscina Exterior': [],
-    'Terreno': [],
-    'Sala de Estar': [],
-    'Sala de Refeições': []
+    'Quarto': [
+        '',
+        ...povoarLista(0, 5, 'Quarto Piso'),
+        ...povoarLista(1, 5, 'Quarto Piso')
+    ],
+    'Wc': [
+        '',
+        ...povoarLista(0, 5, 'Wc Social')
+    ],
+    'Wc Suite': [
+        '',
+        ...povoarLista(0, 5, 'Wc Suite - Quarto Piso'),
+        ...povoarLista(1, 5, 'Wc Suite - Quarto Piso')
+    ],
+    'Varanda': [
+        '',
+        'Varanda - Sala de Estar',
+        'Varanda - Sala de Refeições',
+        'Varanda - Cozinha',
+        ...povoarLista(1, 5, 'Varanda - Piso')
+    ],
+    'Cozinha': ['', 'Cozinha'],
+    'Dispensa': ['', 'Dispensa'],
+    'Corredor': [
+        '',
+        'Corredor Quartos - Piso 0',
+        'Corredor Quartos - Piso 1',
+    ],
+    'Hall de Entrada': [
+        '',
+        'Hall de Entrada - Piso 0',
+        'Hall de Entrada - Piso 1',
+    ],
+    'Lavandaria': ['', 'Lavandaria'],
+    'Terraço': ['', 'Terraço'],
+    'Arrecadação': ['', 'Arrecadação'],
+    'Anexo': ['', 'Anexo'],
+    'Casa das Máquinas': ['', 'Anexo'],
+    'Escritório': ['', 'Escritório'],
+    'Sótão': ['', 'Sótão'],
+    'Escada': ['', 'Escada'],
+    'Garagem': ['', 'Garagem'],
+    'Jardim': ['', 'Jardim'],
+    'Telhado': ['', 'Telhado'],
+    'Telheiro': ['', 'Telheiro'],
+    'Piscina Interior': ['', 'Piscina Interior'],
+    'Piscina Exterior': ['', 'Piscina Exterior'],
+    'Terreno': ['', 'Terreno'],
+    'Sala de Estar': ['', 'Sala de Estar'],
+    'Sala de Refeições': ['', 'Sala de Refeições'],
 }
 
 function povoarLista(ini, lim, texto) {
@@ -47,7 +70,9 @@ async function telaOrcamentos() {
         <div class="painel-despesas">
             <br>
             ${btn('orcamentos', 'Dados de Orçamento', 'formularioOrcamento()')}
-            ${btn('todos', 'Verificar Orçamentos', '')}
+            ${btn('todos', 'Fase 1 - Dados de Orçamento', '')}
+            ${btn('todos', 'Fase 2 - Execuções', '')}
+            ${btn('todos', 'Fase 3 - Orçamento Final', '')}
         </div>
     `
 
@@ -59,7 +84,24 @@ async function formularioOrcamento(idOrcamento) {
     mostrarMenus()
 
     const orcamento = await recuperarDado('dados_orcamentos', idOrcamento)
-    const cliente = {}
+    const clientes = await recuperarDados('dados_clientes')
+
+    const opcoesClientes = Object.entries({ '': { nome: '' }, ...clientes })
+        .map(([idCliente, dados]) => `<option id="${idCliente}">${dados.nome}</option>`)
+        .join('')
+
+    const zonas = (lista, ambiente) => {
+        const opcoes = lista
+            .map(op => `<option>${op}</option>`)
+            .join('')
+
+        return `<select name="${ambiente}">${opcoes}</select>`
+
+    }
+
+    const campos = Object.entries(ambientes)
+        .map(([ambiente, lista]) => modeloLivre(ambiente, zonas(lista, ambiente)))
+        .join('')
 
     const modelo = (texto, valor) => `
         <div style="${vertical}; padding: 10px;">
@@ -68,35 +110,21 @@ async function formularioOrcamento(idOrcamento) {
         </div>
     `
 
-    const zonas = (lista) => {
-        const opcoes = lista
-            .map(op => `<option>${op}</option>`)
-            .join('')
-
-        return `<select>${opcoes}</select>`
-
-    }
-
-    const campos = Object.entries(ambientes)
-        .map(([ambiente, lista]) => modeloLivre(ambiente, zonas(lista)))
-        .join('')
-
     titulo.textContent = 'Dados de Orçamento'
-    const funcao = idOrcamento ? `salvarOrcamento('${idOrcamento}')` : 'salvarOrcamento()'
+
     const acumulado = `
     <div class="cabecalho-clientes">
         ${voltarOrcamentos}
-        <button onclick="${funcao}">Salvar</button>
     </div>
     <div class="painel-clientes">
-        ${modelo('Nome', cliente?.nome || '')}
-        ${modelo('Número de contribuinte', cliente?.moradaFiscal || '')}
-        ${modelo('Morada fiscal', cliente?.moradaExecucao || '')}
-        ${modelo('Morada de Execução', cliente?.numeroContribuinte || '')}
-        ${modelo('Telefone', cliente?.telefone || '')}
-        ${modelo('E-mail', cliente?.email || '')}
-        ${modeloLivre('Data de contato', `<input type="date">`)}
-        ${modeloLivre('Data de visita', `<input type="date">`)}
+        ${modeloLivre('Nome', `<select name="idCliente" onchange="preencherCliente()">${opcoesClientes}</select>`)}
+        ${modeloLivre('Número de contribuinte', `<input name="numeroContribuinte" readOnly>`)}
+        ${modeloLivre('Morada fiscal', `<input name="moradaFiscal" readOnly>`)}
+        ${modeloLivre('Morada de Execução', `<input name="moradaExecucao" readOnly>`)}
+        ${modeloLivre('Telefone', `<input name="telefone" readOnly>`)}
+        ${modeloLivre('E-mail', `<input name="email" readOnly>`)}
+        ${modeloLivre('Data de contato', `<input name="dataContato" type="date">`)}
+        ${modeloLivre('Data de visita', `<input name="dataVisita" type="date">`)}
     </div>
 
     <br>
@@ -106,9 +134,86 @@ async function formularioOrcamento(idOrcamento) {
         <hr style="width: 100%">
         ${campos}
         <hr style="width: 100%">
-        <button>Ir para a Fase 2 - Execuções</button>
+        <button onclick="salvarOrcamento({idOrcamento: ${idOrcamento || false}})">Ir para a Fase 2 - Execuções</button>
     </div>
     `
 
     telaInterna.innerHTML = acumulado
+}
+
+async function preencherCliente() {
+
+    const select = document.querySelector('[name="idCliente"]')
+    const idCliente = select.selectedOptions[0]?.id
+    const cliente = await recuperarDado('dados_clientes', idCliente)
+
+    const campos = ['moradaExecucao', 'moradaFiscal', 'email', 'telefone', 'numeroContribuinte']
+
+    for(const campo of campos) {
+       const el = document.querySelector(`[name="${campo}"]`)
+       if(el) el.value = cliente?.[campo] || ''
+    }
+
+}
+
+async function salvarOrcamento(idOrcamento) {
+
+    idOrcamento = idOrcamento || ID5digitos()
+
+    const select = document.querySelector('[name="idCliente"]')
+    const idCliente = select.selectedOptions[0]?.id 
+
+    if(!idCliente) return popup(mensagem('Campo Cliente obrigatório'), 'Alerta')
+
+    let orcamento = {
+        idCliente,
+        dataVisita: document.querySelector('[name="dataVisita"]').value,
+        dataContato: document.querySelector('[name="dataContato"]').value,
+        zonas: {}
+    }
+
+    for(const ambiente of Object.keys(ambientes)) {
+        const el = document.querySelector(`[name="${ambiente}"]`)
+        if(el && el.value !== '') orcamento.zonas[ambiente] = el.value
+    }
+
+    await inserirDados({[idOrcamento]: orcamento}, 'dados_orcamentos')
+
+
+
+}
+
+async function execucoes(idOrcamento) {
+
+    let orcamento = await recuperarDado('dados_orcamentos', idOrcamento)
+
+    let zonas = orcamento?.zonas || {} //Primeira zona, e seguir para as demais;
+
+    let zona1 = zonas[0]
+
+    const colunas = ['Especialidade', 'Descrição', 'Item1', 'Item2']
+        .map(col => `<th>${col}</th>`)
+        .join('')
+
+    const acumulado = `
+        <div class="painel-clientes">
+            
+            <div class="blocoTabela">
+                <div class="painelBotoes">
+                    <span>${}</span>
+                </div>
+                <div class="recorteTabela">
+                    <table class="tabela">
+                        <thead>
+                            <tr>${colunas}</tr>
+                        </thead>
+                        <tbody id="body"></tbody>
+                    </table>
+                </div>
+                <div class="rodapeTabela"></div>
+            </div>
+
+        </div>
+    `
+    
 }
